8dbdb687db523b14270bd840737f9c8338e5a402
dearettaa@gmail.com <dearettaa@gmail.com>
2019-06-27 15:36:05 +0700

    commit index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+const age = 100;
+
+console.log(age.toString());
+console.log(age.toString(2));
+console.log(age.toString(16));
+
+const number = 123.123123;
+
+console.log(number.toFixed(2));
+console.log(number.toFixed(1));
+console.log(number.toFixed(20));
+
+const digitA = 23.123432124;
+const digitB = 34.765443245;
+
+const formula = digitA * digitB + digitB;
+
+console.log(formula);
+console.log(formula.toFixed(1));s
